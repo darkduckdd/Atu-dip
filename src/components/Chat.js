be2688b723d69cdb6c3c79135463a8ae34504baa
darkduckdd <mr.adilbek75@mail.ru>
2022-04-10 13:56:46 +0600

    chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -91,65 +91,6 @@ const Chat = () => {
         </Grid>
       </Grid>
     </Grid>
-
-    //<Container>
-    /* <Grid
-        container
-        justify={"flex-end"}
-        direction={"column"}
-        width={"187px"}
-        //style={{ height: window.innerHeight - 50, marginTop: 20 }}
-      >
- 
-        <div
-          style={{
-            justify:"flex-end",
-            width: "20%",
-            height: "60vh",
-            border: "1px solid gray",
-            overflowY: "auto",
-          }}
-        >
-          {messages.map((message) => (
-            <div
-              style={{
-                margin: 10,
-                border:
-                  user.uid === message.uid
-                    ? "2px solid green"
-                    : "2px dashed red",
-                marginLeft: user.uid === message.uid ? "auto" : "10px",
-                width: "fit-content",
-                padding: 5,
-              }}
-            >
-              <Grid container>
-                <Avatar src={message.photoURL} />
-                <div>{message.displayName}</div>
-              </Grid>
-              <div>{message.text}</div>
-            </div>
-          ))}
-        </div>
-        <Grid
-          container
-          direction={"column"}
-          alignItems={"flex-end"}
-          style={{ width: "20%", height: "20%" }}
-        >
-          <TextField
-            fullWidth
-            rowsMax={2}
-            variant={"outlined"}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <Button onClick={sendMessage} variant={"outlined"}>
-            Отправить
-          </Button>
-        </Grid>
-      </Grid>
-    //</Container>*/
   );
 };
 
--- src/components/Video/settings.js
+++ src/components/Video/settings.js
@@ -2,7 +2,7 @@ import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
 const appId = "4022bf0d9b1d4fa18e4a55bfe3920a77";
 const token =
-  "0064022bf0d9b1d4fa18e4a55bfe3920a77IAA0V0hTX+JgL1aTabYXqHj4BkqeL4hCW/SQAslQot7+wbSWhMYAAAAAEAAFO07nf2JSYgEAAQB/YlJi";
+  "0064022bf0d9b1d4fa18e4a55bfe3920a77IABdL9ICvb8GTn34l1o3deaNGw10fgWZilt0SC7xjWBBALSWhMYAAAAAEAC5Yzmxz8lTYgEAAQDQyVNi";
 
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token };
 export const useClient = createClient(config);
